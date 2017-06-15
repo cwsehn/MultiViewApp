957e39ff63dc81607eceae27e2942c21b14f8c37
cwsehn <cwsehn@gmail.com>
2017-06-15 10:29:08 -0500

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Chris William Sehnert on 6/15/17.
+//  Copyright © 2017 InSehnDesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Chris William Sehnert on 6/15/17.
+//  Copyright © 2017 InSehnDesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
