1f52b4557ed474b442f04b1c6b51ee633d62877d
cwsehn <cwsehn@gmail.com>
2017-06-15 21:12:34 -0500

    create Playlist class

---
+++ Algorhythm/Playlist.h
@@ -0,0 +1,22 @@
+//
+//  Playlist.h
+//  Algorhythm
+//
+//  Created by Chris William Sehnert on 6/15/17.
+//  Copyright © 2017 InSehnDesigns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UiKit.h>
+
+@interface Playlist : NSObject
+
+@property (strong, nonatomic) NSString *playlistTitle;
+@property (strong, nonatomic) NSString *playlistDescription;
+@property (strong, nonatomic) UIImage *playlistIcon;
+@property (strong, nonatomic) UIImage *playlistIconLarge;
+@property (strong, nonatomic) NSArray *playlistArtists;
+@property (strong, nonatomic) UIColor *backgroundColor;
+
+
+@end
---
+++ Algorhythm/Playlist.m
@@ -0,0 +1,15 @@
+//
+//  Playlist.m
+//  Algorhythm
+//
+//  Created by Chris William Sehnert on 6/15/17.
+//  Copyright © 2017 InSehnDesigns. All rights reserved.
+//
+
+#import "Playlist.h"
+
+@implementation Playlist
+
+
+
+@end
