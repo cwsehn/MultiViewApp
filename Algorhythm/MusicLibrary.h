304da802b044007448652ca72658dc4b5167e37a
cwsehn <cwsehn@gmail.com>
2017-06-15 20:44:47 -0500

    Add Data Model - music library array

---
+++ Algorhythm/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  Algorhythm
+//
+//  Created by Chris William Sehnert on 6/15/17.
+//  Copyright © 2017 InSehnDesigns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong, nonatomic) NSArray *library;
+
+@end
