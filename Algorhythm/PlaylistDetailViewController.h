7220ddef043744cee3483940156ad0e2eaa31ce2
cwsehn <cwsehn@gmail.com>
2017-06-17 09:04:28 -0500

    finishing up detailViewController labeling

--- Algorhythm/PlaylistDetailViewController.h
+++ Algorhythm/PlaylistDetailViewController.h
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-
 @class Playlist;  // note: "forward declaration" ...rather than importing "Playlist.h"...
+
 
 @interface PlaylistDetailViewController : UIViewController
 
@@ -17,8 +17,16 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *playlistCoverImage;
 @property (weak, nonatomic) IBOutlet UILabel *playlistTitle;
-
 @property (weak, nonatomic) IBOutlet UILabel *playlistDescription;
+
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist0;
+
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist1;
+
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist2;
+
+
+
 
 
 @end
